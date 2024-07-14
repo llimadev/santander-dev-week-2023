f0a28e9470b896ec4016890b9ae60d7250d20202
LeonelLima <leonel_mk@yahoo.com.br>
2024-07-13 21:22:01 -0300

    Corrected the readme file by adding correct notation to the lists.

--- src/main/java/me/dio/domain/model/User.java
+++ src/main/java/me/dio/domain/model/User.java
@@ -1,13 +1,29 @@
 package me.dio.domain.model;
 
+import jakarta.persistence.*;
+import org.springframework.data.repository.cdi.Eager;
+
 import java.util.List;
 
+@Entity(name = "tb_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
     private Account account;
+
+    @OneToOne(cascade = CascadeType.ALL)
     private Card card;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Features> features;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<News> news;
 
     public Long getId() {
